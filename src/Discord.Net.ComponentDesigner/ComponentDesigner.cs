d101e62c0fb257f4f681635e20973b170e462a27
Quin Lynch <49576606+quinchs@users.noreply.github.com>
2025-12-23 16:05:49 -0400

    change cx entry exception to be unreachable

--- src/Discord.Net.ComponentDesigner/ComponentDesigner.cs
+++ src/Discord.Net.ComponentDesigner/ComponentDesigner.cs
@@ -8,10 +8,10 @@ public static class ComponentDesigner
     // ReSharper disable once InconsistentNaming
     public static CXMessageComponent cx(
         [StringSyntax("html")] DesignerInterpolationHandler designer
-    ) => throw new InvalidOperationException();
+    ) => throw new UnreachableException("Make sure interceptors are enabled for the component designer, if they are, this is a bug");
 
     // ReSharper disable once InconsistentNaming
     public static CXMessageComponent cx(
         [StringSyntax("html")] string cx
-    ) => throw new InvalidOperationException();
+    ) => throw new UnreachableException("Make sure interceptors are enabled for the component designer, if they are, this is a bug");
 }
